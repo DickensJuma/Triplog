b111add842ea4c47e93cf2ab64394da6f8bb62ef
DickensJuma <dickensjuma13@gmail.com>
2020-09-18 12:45:27 +0300

    webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'fleets#index'
+  resources :fleets, only: [:index, :create] do
+    resources :mileages, only: [:create]
+  end
+
+  devise_for :users
+
+  resources :users, only: [:index, :show]
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
